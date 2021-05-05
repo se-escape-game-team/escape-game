07f897825848aa789148837be8fc9fe366571c94
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2021-05-05 15:30:09 +0200

    Fix proposal

---
+++ escape-game/escape-game/Assets/Scripts/MouseClickBehaviour.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClickBehaviour : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Pressed primary Button.");
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Pressed secondary Button.");
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        //interact with GameObject
+    }
+}
